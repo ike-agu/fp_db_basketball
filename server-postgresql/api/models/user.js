342a55ad0da8ae536061e9e9a24a23434a2a9033
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-03-21 15:44:41 +0000

    added model for user and method to get all

--- server-postgresql/api/models/user.js
+++ server-postgresql/api/models/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const db = require ('../dbConfig');
 const SQL = require('sql-template-strings');//allow you to use ES6 tagged template strings for prepared/escaped statements.
 
@@ -21,28 +20,19 @@ class User{
     })
   }
 
-=======
-const db = require('../db_config/config');
-const SQL = require('sql-template-strings');
-
-class User {
-    constructor(data){
-        this.username = data.username
-        this.email = data.email
-        this.passwordDigest = data.password_digest
-    }
-
-        static get all(){
-        return new Promise(async (res, rej) => {
-            try {
-                let result = await db.run(SQL`SELECT * FROM users;`);
-                let users = result.rows.map(r => new User(r))
-                res(users)
-            } catch (err) {
-                rej(`Error retrieving users: ${err}`)
-            }
-        })
-    }
->>>>>>> d5e5795 (added model for user and method to get all)
+  // static create({ username, email, password }){
+  //       return new Promise(async (res, rej) => {
+  //           try {
+  //               let result = await db.run(SQL`INSERT INTO users (username, email, password_digest)
+  //                                               VALUES (${username}, ${email}, ${password}) RETURNING *;`);
+  //               let user = new User(result.rows[0]);
+  //               res(user)
+  //           } catch (err) {
+  //               rej(`Error creating user: ${err}`)
+  //           }
+  //       })
+  //   }
 
 }
+
+module.exports = User
